a5e72c0d05d5a2a8e0e3789f1029beb089f468c6
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2025-04-24 20:33:37 -0600

    Avance del proyecto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,27 @@
-console.log("Prueba del script desde el archivo JS");
+// 1.- Arreglo que almacena las tareas de mi CRUD
+let tareas = ["Casa", "Trabajo", "Estudio", "Ejercicio"];
+
+// 2.- Vincular el HTML con el JS (DOM)
+const inputTarea = document.getElementById("nueva-tarea");
+const btnAgregar = document.getElementById("btn-agregar");
+const listaTareas = document.getElementById("lista-tareas");
+
+// 3.- Mostrar tolas las tareas en la pantalla
+function mostrarTareas() {
+  listaTareas.innerHTML = "";
+  tareas.forEach((tareasDeLaListaDelArreglo, index) => {
+    const li = document.createElement("li");
+    li.className =
+      "list-group-item d-flex justify-content-between align-items-center";
+    li.innerHTML = `
+      <span>${tareasDeLaListaDelArreglo}</span>
+      <div>
+        <button class="btn btn-warning btn-sm me-2" onclick="editarTarea(${index})">Editar</button>
+        <button class="btn btn-danger btn-sm" onclick="borrarTarea(${index})">Borrar</button>
+      </div>
+    `;
+    listaTareas.appendChild(li);
+  });
+}
+
+mostrarTareas();
